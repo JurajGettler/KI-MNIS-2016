cafe02c73ae99be5b39e99b3c49dc6c92439c39c
JurajGettler <juraj.gettler@student.ukf.sk>
2016-03-29 22:04:24 +0200

    UDAJE O POUZIVAELOVI

--- Nastavenia.java
+++ Nastavenia.java
@@ -1,53 +1,111 @@
 package orthos.fyzicke_zatazenie;
 
-import android.app.AlertDialog;
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Nastavenia extends AppCompatActivity {
 
     DatabaseHelper myDb;
-    Button zobraz;
+    Spinner pohlavie_edit;
+    String[] zozmam_pohlavi = {"MUZSKE","ZENSKE"};
+    int [] image_pohlavie = {R.drawable.male,R.drawable.female};
+    int vybrate_pohlavie;
+    Button uprav;
+    TextView meno,pohlavie,vaha,max_tep;
+    EditText meno_edit,vaha_edit,max_tep_edit;
+    Cursor res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nastavenia);
         myDb = new DatabaseHelper(this);
-        zobraz = (Button) findViewById(R.id.zobraz);
 
-       }
+        res = myDb.getAllDataUSER();
+        uprav = (Button) findViewById(R.id.user_uprav);
+        meno = (TextView) findViewById(R.id.user_meno);
+        pohlavie = (TextView) findViewById(R.id.user_pohlavie);
+        vaha = (TextView) findViewById(R.id.user_vaha);
+        max_tep = (TextView) findViewById(R.id.user_max_tep);
+        meno_edit = (EditText) findViewById(R.id.user_meno_edit);
+        vaha_edit = (EditText) findViewById(R.id.user_vaha_edit);
+        max_tep_edit = (EditText) findViewById(R.id.user_max_tep_edit);
 
-    public void zobrazM (View view){
-        Cursor res = myDb.getAllGPSdata();
-        if (res.getCount() == 0){
+        if (res.moveToFirst()){
+            meno.setText("MENO : " + res.getString(1));
+            pohlavie.setText("POHLAVIE : " + res.getString(2));
+            vaha.setText("VAHA : " + res.getString(3) + " Kg");
+            max_tep.setText("MAX TEPOVA FREKVENCIA : " + res.getString(4));
 
-            showMessage("Error","NENASLI SA DATA");
-
-            return;
-        }else{
-            StringBuffer buffer = new StringBuffer();
-            while (res.moveToNext()){
-                buffer.append("ID : " + res.getString(0) + "\n");
-                buffer.append("ID TRENINGU : " + res.getString(1) + "\n");
-                buffer.append("LATITUDE : " + res.getString(2) + "\n");
-                buffer.append("LONGITUDE : " + res.getString(3) + "\n\n");
-            }
-
-            showMessage("Data",buffer.toString());
+            meno_edit.setText(res.getString(1));
+            vaha_edit.setText(res.getString(3));
+            max_tep_edit.setText(res.getString(4));
         }
+
+        pohlavie_edit = (Spinner) findViewById(R.id.user_pohlavie_edit);
+        SpinerAdapter spinerAdapter = new SpinerAdapter(this,zozmam_pohlavi,image_pohlavie);
+        pohlavie_edit.setAdapter(spinerAdapter);
+        pohlavie_edit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+                                            @Override
+                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                vybrate_pohlavie = position;
+
+
+                                            }
+
+                                            @Override
+                                            public void onNothingSelected(AdapterView<?> parent) {
+
+                                            }
+                                        }
+        );
+
+
     }
 
-    public void showMessage (String title,String message){
+    public void UPRAV (View view){
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
+        if (!res.moveToNext()){
+            String m =meno_edit.getText().toString();
+            String p ;
+            if (vybrate_pohlavie == 0){
+                p = "MUZSKE";
+            }else {p = "ZENSKE";}
+            String v = vaha_edit.getText().toString();
+            String t = max_tep_edit.getText().toString();
+            myDb.insertDataUser(m,p,v,t);
+            Intent intent = new Intent(getApplicationContext(),Uvod.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            Toast.makeText(Nastavenia.this,"UDAJE ZMENENE",Toast.LENGTH_LONG).show();
+
+        }else {
+            String m =meno_edit.getText().toString();
+            String p ;
+            if (vybrate_pohlavie == 0){
+                p = "MUZSKE";
+            }else {p = "ZENSKE";}
+            String v = vaha_edit.getText().toString();
+            String t = max_tep_edit.getText().toString();
+            myDb.ubdateDataUser(m,p,v,t);
+            Intent intent = new Intent(getApplicationContext(),Uvod.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            Toast.makeText(Nastavenia.this,"UDAJE ZMENENE",Toast.LENGTH_LONG).show();}
+
+
 
     }
+
 }
